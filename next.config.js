7f4c84efc7ec744bcfa2f2efc1ab5d61fe4ecde0
FayssalProg <81432712+FayssalProg@users.noreply.github.com>
2025-11-14 12:47:23 -0500

    gh changes

--- next.config.js
+++ next.config.js
@@ -4,4 +4,12 @@ const nextConfig = {
   swcMinify: true,
 }
 
-module.exports = nextConfig
+module.exports = {
+  // when deployed to https://fayssalprog.github.io/MyPortfolio
+  basePath: '/MyPortfolio',
+  assetPrefix: '/MyPortfolio/',
+  trailingSlash: true, // recommended for GitHub Pages to produce directory/index.html structure
+  images: {
+    unoptimized: true // avoid next/image optimizations that break static export
+  }
+};
